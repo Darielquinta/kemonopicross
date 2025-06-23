3f8e3b906aabdb1bf710b77307b51cddce5b2104
Dariel Quintanilla <darielquinta@gmail.com>
2025-06-23 13:57:54 -0400

    Update build script in package.json and change base path in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "/picross/",
+  base: "/",
   envDir: '../',
   server: {
     host: true,                 // listen on all interfaces
