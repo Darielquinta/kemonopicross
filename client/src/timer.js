5ddfcd9b147129387c9eb9a8485ebab8c0fe891d
Dariel Quintanilla <darielquinta@gmail.com>
2025-06-24 15:54:08 -0400

    feat: integrate Discord leaderboard functionality and update timer handling

--- client/src/timer.js
+++ client/src/timer.js
@@ -14,6 +14,7 @@ export function createTimer(update) {
   const stopTimer = () => {
     clearInterval(handle);
     active = false;
+    return Date.now() - start;
   };
   return { startTimer, stopTimer };
 }
